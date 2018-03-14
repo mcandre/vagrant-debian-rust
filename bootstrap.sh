a386904d9bfc39a6af97d9f51f81d0d0953b114d
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-03-13 19:20:27 -0500

    fix cargo

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 sudo apt-get update &&
     sudo apt-get install -y curl ca-certificates build-essential &&
     su vagrant -c 'curl https://sh.rustup.rs -sSf | sh -s -- -y' &&
-    sudo apt-get purge -y curl ca-certificates &&
+    sudo apt-get purge -y curl &&
     sudo apt-get autoremove --purge -y &&
     sudo apt-get autoclean -y &&
     sudo apt-get clean -y &&
